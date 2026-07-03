09d610518706a3272eb02e8a1f08631855291e1a
sherin susan <sherinsusan31@gmail.com>
2026-07-03 14:30:23 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -99,7 +99,7 @@ st.set_page_config(page_title="PragyanAI Video Creator", layout="wide")
 if os.path.exists("sherin.png"):
     st.image("sherin.png")
 
-st.title("PragyanAI - Multimedia Merger")
+st.title("sherin- Multimedia Merger")
 st.markdown("Upload multiple images, specify timing, and add audio from a file or YouTube.")
 
 with st.sidebar:
